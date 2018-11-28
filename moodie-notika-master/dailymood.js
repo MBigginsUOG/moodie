35120c2f142287223cf7d7f641ff86e849b9c50b
Matthew <2135573b@student.gla.ac.uk>
2018-11-28 12:51:26 +0000

    Added dailymood js

---
+++ moodie-notika-master/dailymood.js
@@ -0,0 +1,134 @@
+var freqData=[
+{day:'M', freq:{sentiment_very_dissatisfied:3, sentiment_dissatisfied:7, sentiment_neutral:10, sentiment_satisfied:15, sentiment_very_satisfied:25}},
+{day:'T', freq:{sentiment_very_dissatisfied:0, sentiment_dissatisfied:3, sentiment_neutral:9, sentiment_satisfied:13, sentiment_very_satisfied:15}},
+{day:'W', freq:{sentiment_very_dissatisfied:0, sentiment_dissatisfied:1, sentiment_neutral:10, sentiment_satisfied:15, sentiment_very_satisfied:19}},
+{day:'T', freq:{sentiment_very_dissatisfied:3, sentiment_dissatisfied:5, sentiment_neutral:15, sentiment_satisfied:13, sentiment_very_satisfied:16}},
+{day:'F', freq:{sentiment_very_dissatisfied:4, sentiment_dissatisfied:2, sentiment_neutral:5, sentiment_satisfied:3, sentiment_very_satisfied:10}},
+{day:'S', freq:{sentiment_very_dissatisfied:5, sentiment_dissatisfied:2, sentiment_neutral:10, sentiment_satisfied:17, sentiment_very_satisfied:20}},
+{day:'S', freq:{sentiment_very_dissatisfied:0, sentiment_dissatisfied:1, sentiment_neutral:2, sentiment_satisfied:6, sentiment_very_satisfied:5}},
+];
+
+// dashboard('#dashboard',freqData);
+dashboard('#histId','#legId', freqData);
+
+// function dashboard(id, fData){
+function dashboard(histId, legId, fData){
+
+  var	margin = {top: 30, right: 40, bottom: 70, left: 50};
+
+  var barColor = '#2196F3';
+  function segColor(c){ return {
+                                  sentiment_very_dissatisfied:"#e21818",
+                                  sentiment_dissatisfied:"#FB8C00",
+                                  sentiment_neutral:"#fbd300",
+                                  sentiment_satisfied:"#79e65e",
+                                  sentiment_very_satisfied:"#298e00"
+                                }[c];
+                        }
+  // compute total for each Activity.
+  fData.forEach(function(d){
+                            d.total = (d.freq.sentiment_very_dissatisfied + d.freq.sentiment_dissatisfied + d.freq.sentiment_neutral + d.freq.sentiment_satisfied + d.freq.sentiment_very_satisfied;
+                            });
+
+    // function to handle histogram.
+  function histoGram(fD){
+    var hG={};
+    var hGDim = {t: 60, r: 0, b: 30, l: 0};
+    hGDim.w = 500 - hGDim.l - hGDim.r,
+    hGDim.h = 300 - hGDim.t - hGDim.b;
+
+    //create svg for histogram.
+    // var hGsvg = d3.select(id).append("svg")
+    // // var hGsvg = d3.select(histId).append("svg")
+    //             .attr("width", hGDim.w + hGDim.l + hGDim.r)
+    //             .attr("height", hGDim.h + hGDim.t + hGDim.b).append("g")
+    //             .attr("transform", "translate(" + hGDim.l + "," + hGDim.t + ")");
+
+    var hGsvg = d3.select(histId)
+    // var hGsvg = d3.select(id)
+      .append("div")
+      .classed("svg-container", true)
+      .append("svg")
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr("viewBox", "0 0 600 400")
+      .classed("svg-content-responsive", true)
+      .attr("transform", "translate(" + hGDim.l + "," + hGDim.t + ")")
+      .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")")
+      .style("font-size","35px");
+
+
+        // create function for x-axis mapping.
+        var x = d3.scale.ordinal().rangeRoundBands([0, hGDim.w], 0.1)
+                .domain(fD.map(function(d) { return d[0]; }));
+
+        // Add x-axis to the histogram svg.
+        hGsvg.append("g").attr("class", "x axis")
+            .attr("transform", "translate(0," + hGDim.h + ")")
+            .call(d3.svg.axis().scale(x).orient("bottom"))
+            .style("font-size","22px");
+
+        // Create function for y-axis map.
+        var y = d3.scale.linear().range([hGDim.h, 0])
+                .domain([0, d3.max(fD, function(d) { return d[1]; })]);
+
+        // Create bars for histogram to contain rectangles and freq labels.
+        var bars = hGsvg.selectAll(".bar").data(fD).enter()
+                .append("g").attr("class", "bar");
+
+        //create the rectangles.
+        bars.append("rect")
+            .attr("x", function(d) { return x(d[0]); })
+            .attr("y", function(d) { return y(d[1]); })
+            .attr("width", x.rangeBand())
+            .attr("height", function(d) { return hGDim.h - y(d[1]); })
+            .attr('fill',barColor)
+            .on("mouseover",mouseover)// mouseover is defined bebad.
+            .on("mouseout",mouseout);// mouseout is defined bebad.
+
+        //Create the frequency labels above the rectangles.
+        bars.append("text").text(function(d){ return d3.format(",")(d[1])})
+            .attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
+            .attr("y", function(d) { return y(d[1])-5; })
+            .attr("text-anchor", "middle");
+
+        function mouseover(d){  // utility function to be called on mouseover.
+            // filter for selected Activity.
+            var st = fData.filter(function(s){ return s.Activity == d[0];})[0],
+                nD = d3.keys(st.freq).map(function(s){ return {type:s, freq:st.freq[s]};});
+
+            // call update functions of pie-chart and legend.
+            pC.update(nD);
+            leg.update(nD);
+        }
+
+        function mouseout(d){    // utility function to be called on mouseout.
+            // reset the pie-chart and legend.
+            pC.update(tF);
+            leg.update(tF);
+        }
+
+        // create function to update the bars. This will be used by pie-chart.
+        hG.update = function(nD, color){
+            // update the domain of the y-axis map to reflect change in frequencies.
+            y.domain([0, d3.max(nD, function(d) { return d[1]; })]);
+
+            // Attach the new data to the bars.
+            var bars = hGsvg.selectAll(".bar").data(nD);
+
+            // transition the height and color of rectangles.
+            bars.select("rect").transition().duration(500)
+                .attr("y", function(d) {return y(d[1]); })
+                .attr("height", function(d) { return hGDim.h - y(d[1]); })
+                .attr("fill", color);
+
+            // transition the frequency labels location and change value.
+            bars.select("text").transition().duration(500)
+                .text(function(d){ return d3.format(",")(d[1])})
+                .attr("y", function(d) {return y(d[1])-5; });
+        }
+        return hG;
+    }
+
+}
